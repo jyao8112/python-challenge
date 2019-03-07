dca9f57d59c3cb9abfda6770373e75ea653fa141
jyao8112 <47203211+jyao8112@users.noreply.github.com>
2019-03-06 16:41:22 -0800

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,9 @@ total_vote = 0
 max_vote = 0
 
 electiondata_csv = os.path.join(".","Resource","election_data.csv")
+outputdata_txt = os.path.join("output_data.txt")
 
+### load data into dictionary
 with open (electiondata_csv,"r",newline="") as csvfile:
    csvreader = csv.reader(csvfile, delimiter=',')
    next(csvfile)
@@ -14,20 +16,29 @@ with open (electiondata_csv,"r",newline="") as csvfile:
    for row in csvreader:
       total_vote = total_vote + 1
       d[row[2]] = d.get(row[2], 0) + 1
-print("Election Results")
-print("------------------------------")  
-print("Total vote: ", total_vote)
-print("------------------------------") 
- 
-for key in d:
-   percentage = round(d[key] / total_vote,2)
-   if d[key] > max_vote:
-      max_vote = d[key]
-      max_key = key
-   print(key+":"+"{:0%}".format(percentage)+"("+str(d[key])+")")
 
+### write results to file
+with open (outputdata_txt,"w") as txtfile:
+   txtfile.writelines("Election Results\n")
+   txtfile.writelines("------------------------------\n")
+   txtfile.writelines("Total vote: " + str(total_vote) + "\n")
+   txtfile.writelines("------------------------------\n")
+   txtfile.writelines('Finacial Analysis\n')
+   txtfile.writelines("------------------------------\n")
+   for key in d:
+      percentage = round(d[key] / total_vote, 3)
+      if d[key] > max_vote:
+         max_vote = d[key]
+         max_key = key
+      txtfile.writelines(key+":"+"{:.3%}".format(percentage)+"("+str(d[key])+")\n")
+      #txtfile.writelines(key+":"+"{:.6%}".format(percentage)+"("+str(d[key])+")\n")
 
-print("------------------------------")  
+   txtfile.writelines("------------------------------\n")
+   txtfile.writelines("Winner is:" + max_key + "\n")
+   txtfile.writelines("------------------------------")
 
-print("Winner is:"+ max_key)
-print("------------------------------")  
+### print file
+with open (outputdata_txt,"r") as outputfile:
+   print(outputfile.read())
+
+   
